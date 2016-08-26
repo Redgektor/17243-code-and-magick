1815f5de26d8bbb24a737b94c0aeaa05ccd410d1
Andrey Karamnoff <karamnoff@gmail.com>
2016-08-26 19:26:33 +0300

    :sparkles: Create object gallery via constractor to display pictures

--- src/main.js
+++ src/main.js
@@ -4,6 +4,22 @@ require('./check');
 require('./reviews');
 var form = require('./form');
 var Game = require('./game');
+var Gallery = require('./gallery');
+
+var linksList = document.querySelectorAll('.photogallery a');
+var picturesList = document.querySelectorAll('.photogallery a > img');
+
+var picturesURL = Array.prototype.map.call(picturesList, function(elem) {
+  return elem.getAttribute('src');
+});
+
+var gallery = new Gallery(picturesURL);
+
+Array.prototype.forEach.call(linksList, function(elem, i) {
+  elem.addEventListener('click', function() {
+    gallery.show(i);
+  });
+});
 
 var game = new Game(document.querySelector('.demo'));
 game.initializeLevelAndStart();
